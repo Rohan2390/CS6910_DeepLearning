5edaf0382be85ec043839df3094271377730dfa3
Rohan2390 <91365377+Rohan2390@users.noreply.github.com>
2022-02-21 23:14:17 +0530

    Update optimizers.py
    added momemtum gradient descent

--- Assn_1/optimizers.py
+++ Assn_1/optimizers.py
@@ -11,3 +11,28 @@ class SGD:
 
             self.nn.weights[i] = self.nn.weights[i] - self.lr*self.nn.wUpdate[i]
             self.nn.bias[i] = self.nn.bias[i] - self.lr*self.nn.bUpdate[i]
+            
+           
+class MOMEMTUM:
+
+    def __init__(self,nn,gamma=0.9,lr=0.001):
+
+        self.nn = nn
+        self.lr = lr
+        self.gamma = gamma
+        
+        self.lastUpdateW =  [np.zeros_like(i) for i in nn.weights]
+        self.lastUpdateB =  [np.zeros_like(i) for i in nn.weights]
+
+
+    def update(self):
+
+        for i in range(self.nn.num_of_layers-1):
+
+            
+            self.lastUpdateW[i] = (gamma * self.lastUpdateW[i]) + (self.lr * self.nn.wUpdate[i])
+            self.nn.weights[i] = self.nn.weights[i] - self.lastUpdateW[i] 
+            
+            
+            self.lastUpdateB[i] = (gamma * self.lastUpdateB[i]) + (self.lr * self.nn.bUpdate[i])
+            self.nn.bias[i] = self.nn.bias[i] - self.lastUpdateB[i]          
