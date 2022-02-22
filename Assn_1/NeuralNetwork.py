dataPreparation.py added normalization, add one hot encoding,fixed plotting bugs.
Changed parameter initialization, bugs solved.

Utils bugs solved

Added trainer class

Assn_1/NeuralNetwork.py
@@ -21,8 +21,8 @@ class NeuralNetwork():
     self.weights = []
     self.bias = []
     for i in range(len(layers) - 1):
-      self.weights.insert(i, (np.random.rand(layers[i + 1], layers[i])))
-      self.bias.insert(i, (np.random.rand(layers[i + 1], 1)))
+      self.weights.insert(i, (np.random.uniform(-0.2,0.2,(layers[i], layers[i+1]))))
+      self.bias.insert(i, (np.random.uniform(-0.2,0.2,(layers[i + 1],))))
 
   def ForwardProp(self, X):
     self.a = {}
@@ -31,8 +31,8 @@ class NeuralNetwork():
     self.a[0] = X
 
     for i, (w, b, af) in enumerate(zip(self.weights, self.bias, self.actFun)):
-      self.a[i + 1] = np.matmul(self.h[1],w) + b
-      self.h[i + 1] = af(self.a[i])
+      self.a[i + 1] = np.matmul(self.h[i],w) + b
+      self.h[i + 1] = af(self.a[i+1])
 
     return self.h, self.a, self.h[i + 1]
 
@@ -42,10 +42,10 @@ class NeuralNetwork():
     self.wUpdate = {}
     self.bUpdate = {}
 
-    for i in range(self.numOfLayers-1,0,-1):
+    for i in range(self.numOfLayers-2,-1,-1):
+      self.wUpdate[i] = np.mean(aGrad*np.expand_dims(self.h[i],axis=2),axis=0)
+      self.bUpdate[i] = np.mean(aGrad,axis=(0,1))
 
-      self.wUpdate[i] = np.mean(aGrad*np.expand_dims(self.h[i-1],axis=2),axis=0)
-      self.bUpdate[i] = np.mean(aGrad,axis=0)
-
-      hGrad = np.matul(self.weights[i],aGrad)
-      aGrad = hGrad*actFunDerivative[self.actFun[i-1]](self.a[i-1])
+      if i>0:
+        hGrad = np.matmul(aGrad,self.weights[i].T)
+        aGrad = hGrad*actFunDerivative[self.actFun[i-1]](np.expand_dims(self.a[i],axis=1))

Assn_1/Utils.py
@@ -7,7 +7,8 @@ def SigmoidDerivative(x):
     return x * (1 - x)
 
 def Softmax(x):
-    return np.exp(x) / sum(np.exp(x))
+    exp = np.exp(x)
+    return exp / np.sum(exp,axis=1).reshape(-1,1)
 
 def Tanh(x):
     return np.tanh(x)
@@ -35,6 +36,7 @@ class CrossEntropyLoss:
         pass
 
     def CalculateLoss(self,yTrue,yPreds):
+
         self.lossVal = np.mean(-1*yTrue*np.log(yPreds))
         self.lossGradientVal = yPreds-yTrue
         self.lossGradientVal = self.lossGradientVal.reshape((-1,1,len(yTrue[0])))

Assn_1/dataPreparation.py
@@ -13,9 +13,12 @@ def PrePareMNISTData():
     xTrain = xTrain.reshape(-1,28*28) #Add this to config
     xTest = xTest.reshape(-1,28*28) #Add this to config
 
+    yTrain = np.eye(10)[yTrain]
+    yTest = np.eye(10)[yTest]
+
     (xTrain,yTrain),(xVal,yVal) = splitData(xTrain,yTrain)
 
-    return (xTrain,yTrain),(xVal,yVal),(xTest,yTest)
+    return (xTrain/255.0,yTrain),(xVal/255.0,yVal),(xTest/255,yTest)
 
 def splitData(x,y,percent=10):
 
@@ -29,7 +32,11 @@ def PlotData(x,y):
 
     classes = np.unique(y)
 
-    fig,ax = plt.subplot(1,len(classes),figsize=(20,40))
+    fig,ax = plt.subplots(1,len(classes),figsize=(20,40))
 
     for i in classes:
-        ax[i].imshow(x[y[y==classes]][0])
+        ax[i].imshow(x[y==i][0])
+
+if __name__=='__main__':
+    PrePareMNISTData()
+    plt.show()