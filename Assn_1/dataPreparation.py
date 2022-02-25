5aa4658aca101e65898565a7533dbeff55807151
Kush <cs21m030@smail.iitm.ac.in>
2022-02-25 18:51:22 +0530

    Wandb Integration

--- Assn_1/dataPreparation.py
+++ Assn_1/dataPreparation.py
@@ -1,14 +1,13 @@
 from keras.datasets import fashion_mnist
 import matplotlib.pyplot as plt
 import numpy as np
+import wandb
 
-#Normalization may be added in future
-
-def PrePareMNISTData():
+def PrePareMNISTData(wandbLog=False):
 
     (xTrain,yTrain),(xTest,yTest) = fashion_mnist.load_data()
 
-    PlotData(xTrain,yTrain)
+    PlotData(xTrain,yTrain,wandbLog=wandbLog)
 
     xTrain = xTrain.reshape(-1,28*28) #Add this to config
     xTest = xTest.reshape(-1,28*28) #Add this to config
@@ -28,14 +27,19 @@ def splitData(x,y,percent=10):
     return (x[trainIndex],y[trainIndex]),(x[valIndex],y[valIndex])
 
 
-def PlotData(x,y):
+def PlotData(x,y,wandbLog=False):
 
     classes = np.unique(y)
 
-    fig,ax = plt.subplots(1,len(classes),figsize=(20,40))
+    fig,ax = plt.subplots(1,len(classes),figsize=(10,10))
 
     for i in classes:
         ax[i].imshow(x[y==i][0])
+
+    if wandbLog:
+        wandb.log({'Data Plot': plt})
+    else:
+        plt.savefig('DataPlot.png')
 
 if __name__=='__main__':
     PrePareMNISTData()
