d7f94b2818a2e17ec0e6fbdcafbe89db86c9f0a1
Kush <cs21m030@smail.iitm.ac.in>
2022-03-22 01:22:06 +0530

    Error solved

--- Assn_2/dataPreparation.py
+++ Assn_2/dataPreparation.py
@@ -4,46 +4,44 @@ import zipfile
 from tqdm import tqdm
 import argparse
 
-def main(datasetPath):
 
-    trainPath = 'inaturalist_12k'+os.sep+'train'
-    validPath = 'inaturalist_12k'+os.sep+'valid'
-    testPath = 'inaturalist_12k'+os.sep+'val'
+def main(datasetPath):
+    trainPath = os.path.join('inaturalist_12K', 'train')
+    validPath = os.path.join('inaturalist_12K', 'valid')
+    testPath = os.path.join('inaturalist_12K', 'val')
 
     if not os.path.exists(datasetPath) and not (os.path.exists(trainPath) and os.path.exists(testPath)):
-
         print('Zip file not found\nExiting Program')
         return
 
     if not (os.path.exists(trainPath) and os.path.exists(testPath)):
         print('Extracting Zip')
 
-        with zipfile.ZipFile(datasetPath,'r') as z1:
-
+        with zipfile.ZipFile(datasetPath, 'r') as z1:
             z1.extractall('')
 
-    classes = [i for i in os.listdir(trainPath) if i[0]!='.']
+    classes = [i for i in os.listdir(trainPath) if i[0] != '.']
 
     if not os.path.exists(validPath):
         print('Creating Validation Set')
         os.makedirs(validPath)
 
-        x,y = [],[]
+        x, y = [], []
 
         for c in tqdm(classes):
-            os.makedirs(os.path.join(validPath,c))
+            os.makedirs(os.path.join(validPath, c))
 
-            names = os.listdir(os.path.join(trainPath,c))
-            x+=names
-            y+=[c]*len(names)
+            names = os.listdir(os.path.join(trainPath, c))
+            x += names
+            y += [c] * len(names)
 
-        trainX,valX,trainY,valY = train_test_split(x,y,stratify=y,test_size=0.1)
+        trainX, valX, trainY, valY = train_test_split(x, y, stratify=y, test_size=0.1)
 
-        for i,j in zip(valX,valY):
-            os.rename(os.path.join(trainPath,j,i),os.path.join(validPath,j,i))
-            
-            
-if __name__=='__main__':
+        for i, j in zip(valX, valY):
+            os.rename(os.path.join(trainPath, j, i), os.path.join(validPath, j, i))
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Data Preparation')
-    parser.add_argument('--path',dest='path',type=str,help='Path to the dataset')
+    parser.add_argument('--path', dest='path', type=str, help='Path to the dataset')
     main((parser.parse_args().path))
