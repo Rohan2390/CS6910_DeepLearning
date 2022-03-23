d508ba8268c3b66347d2f601509a8903d3562dae
Kush <cs21m030@smail.iitm.ac.in>
2022-03-23 17:35:42 +0530

    Error solved

--- Assn_2/PartB/wandbmain.py
+++ Assn_2/PartB/wandbmain.py
@@ -1,15 +1,14 @@
-import argparse
-
 from main import train
 import wandb
 import json
+import argparse
 
-def wandbTrain(config):
+def wandbTrain(config=None):
     with wandb.init(config=config):
         config = wandb.config
-        train(config, wandbLog=True)
+        train(config=config,wandbLog=True)
 
-def main(wandnConfig):
+def main(wandbConfig):
     wandb.init(project='DL Assignment 2',entity='ed21s001_cs21m030')
 
     sweep_config = {
@@ -20,7 +19,7 @@ def main(wandnConfig):
         },
     }
 
-    sweep_config['parameters'] = wandnConfig
+    sweep_config['parameters'] = wandbConfig
     sweep_id = wandb.sweep(sweep_config, project="DL Assignment 2")
     print(f"Sweep ID:{sweep_id}")
     wandb.agent(sweep_id, wandbTrain)
@@ -30,6 +29,14 @@ if __name__=='__main__':
 
     parser = argparse.ArgumentParser(description='Wandb Training')
     parser.add_argument('--path', dest='path', type=str, help='Path to the wandb config json file')
+    args = parser.parse_args()
 
-    with open(parser.parse_args().path, 'r') as f:
+    if args.path==None:
+        path = 'config.json'
+    else:
+        path = args.path
+
+    with open(path, 'r') as f:
         wandbConfig = json.load(f)
+
+    main(wandbConfig)
